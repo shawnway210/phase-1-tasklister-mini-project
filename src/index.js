47222b50dd08d99a2d8d9e69d1493c3169a8f55f
shawnconway <shawnway210@gmail.com>
2023-08-23 16:30:48 -0400

    Todo List

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  console.log("DOM content loaded")
+  const form =document.querySelector(`form`)
+  console.log("Form element: ", form)
+  form.addEventListener(`submit`, (e) => {
+    e.preventDefault()
+    console.log("Form submitted")
+    const descriptionInput = e.target.querySelector("#new-task-description")
+    console.log("Description input: ", descriptionInput)
+    buildNewTask(descriptionInput.value)
+
+  })
+    
+})
+
+function buildNewTask(description){
+  console.log("Task description: ", description)
+  let p = document.createElement('p')
+  let btn = document.createElement('Button')
+  btn.addEventListener(`click`, (e) => handleDelete(e))
+  btn.textContent = 'delete'
+  p.textContent = `${description}  `
+  p.appendChild(btn)
+  console.log(p)
+  let tasksList = document.querySelector("#tasks")
+  tasksList.appendChild(p)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
